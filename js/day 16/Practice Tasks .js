22ca4fb6d187fdf4ea7474936482ca44b76090fc
aaronannadurai66-dotcom <aaronannadurai66@gmail.com>
2025-12-15 10:58:20 +0530

    Create Practice Tasks .js

---
+++ js/day 16/Practice Tasks .js
@@ -0,0 +1,11 @@
+console.log("My name is Aaron");
+
+// This is the first single-line comment
+// This is the second single-line comment
+
+/*
+   This is a multi-line comment
+   written in JavaScript
+*/
+
+console.log("External script.js file is working!");
