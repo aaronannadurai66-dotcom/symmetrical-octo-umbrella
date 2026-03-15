5dcb8926231ec14f2c3ffcc23f5df7dffcb2365d
aaronannadurai66-dotcom <aaronannadurai66@gmail.com>
2026-03-16 00:26:33 +0530

    done

---
+++ JAVA/javaProgram/src/Assignment3/src/project/Positive.java
@@ -0,0 +1,21 @@
+package project;
+
+public class Positive {
+    public static void main(String[] args) {
+
+        int num = -5;
+
+        if (num > 0) {
+            System.out.println("Number is Positive");
+        } 
+        else if (num < 0) {
+            System.out.println("Number is Negative");
+        } 
+        else {
+            System.out.println("Number is Zero");
+        }
+
+    }
+
+
+}
---
+++ JAVA/javaProgram/src/Assignment3/src/project/largesttwo.java
@@ -0,0 +1,18 @@
+package project;
+
+public class largesttwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println("Largest number is: " + a);
+        } else {
+            System.out.println("Largest number is: " + b);
+        }
+
+	}
+
+}
